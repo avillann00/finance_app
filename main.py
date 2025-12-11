4ec1ecfc81fdfc42f44812fc51c86df5ccf2bd42
AustinVillanueva <avillann00@gmail.com>
2025-12-11 17:24:55 -0500

    feat(Requirements): Updated requirements

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ app = create_app()
 
 # if this file is file is executed then run the app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True, port=5000)
